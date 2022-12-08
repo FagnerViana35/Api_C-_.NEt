2d3cdb988382d727b4c1b919d1bf16f1206c7b0e
Fagner Viana <fagner.viana@domvsit.com.br>
2022-12-08 08:51:17 -0300

    Primeiro Commit

---
+++ Entidades/Entidades/Noticia.cs
@@ -0,0 +1,42 @@
+﻿using Entidades.Notificacoes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Entidades
+{
+    //Serve para o banco criar com o nome que eu colocar [Table("TB_NOTICIA")]
+    [Table("TB_NOTICIA")]
+    //Erdou a classe Notifica
+    internal class Noticia : Notifica
+    {
+        [Column("NTC_ID")]
+        public string Id { get; set; }
+
+        [Column("NTC TITULO")]
+        [MaxLength(255)]
+        public string? Titulo { get; set; }
+
+        [Column("NTC_INFORMACAO")]
+        public string? Informacao { get; set; }
+
+        [Column("NTC_ATIVO")]
+        public bool Ativo { get; set; }
+
+        [Column("NTC_DATA_CADASTRO")]
+        public DateTime DataCadastro { get; set; }
+
+        [Column("NTC_DATA_ALTERACAO")]
+        public DateTime DataAlteracao { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        [Column(Order = 1)]
+
+        public string? UserId {get; set;}
+        public virtual ApplicationUser? ApplicationUser { get; set; }
+    }
+}
---
+++ Entidades/Notificacoes/Notifica.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Notificacoes
+{
+    internal class Notifica
+    {
+        //Construtor para não dar erro
+        public Notifica() 
+
+        {
+            //Iniciado a propriedade de lista para não dar erro
+            Notificacoes = new List<Notifica>(); 
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string Mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifica> Notificacoes;
+        
+        //Válida se dado mandado para API é uma string
+        public bool ValidaPropriedadeString(string valor, string nomePropriedade)
+        {
+            if(string.IsNullOrWhiteSpace(valor)  || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifica
+                {
+                    Mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade
+                });
+                return false;
+            }
+                return true;
+        }
+
+        //Válida se dado mandado para API é uma maior de zero e não possui espaço em branco
+        public bool ValidaPropriedadeSDecimal(decimal valor, string nomePropriedade)
+        {
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifica
+                {
+                    Mensagem = "Valor deve ser maior que 0",
+                    NomePropriedade = nomePropriedade
+                });
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
+
